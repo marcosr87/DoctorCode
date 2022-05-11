1571f8b4f79bed10f473d70b0bfd616b66e2ac2d
marcosr87 <marcosr_87@hotmail.com>
2022-05-10 21:43:17 -0300

    NewUser

--- js/RegisterDoctor.js
+++ js/RegisterDoctor.js
@@ -1,11 +1,13 @@
-class Doctor {
-  constructor(name, surname, email, license, speciality, password ){
+class User {
+  constructor(name, surname, email, license, speciality, password, isAprobed, isDoctor ){
     this.name = name;
     this.surname = surname;
     this.email = email;
     this.license = license;
     this.speciality = speciality;
-    this.password = password
+    this.password = password;
+    this.isAprobed = isAprobed;
+    this.isDoctor = isDoctor
   }
 }
 
@@ -16,20 +18,27 @@ let licenseHTML = document.getElementById('license');
 let specialityHTML = document.getElementById('floatingSelect');
 let passwordHTML = document.getElementById('password');
 
-function newDoctor() {
-  let nameDoc = nameHTML.value;
-  let surnameDoc = surnameHTML.value;
-  let emailDoc = emailHTML.value;
-  let licenseDoc = licenseHTML.value;
-  let specialityDoc = specialityHTML.options[specialityHTML.selectedIndex].text;
-  let passwordDoc = passwordHTML.value;
-  let newDoc = new Doctor(nameDoc,surnameDoc,emailDoc,licenseDoc,specialityDoc,passwordDoc);
-  console.log(newDoc);
-  return newDoc
+function newUser() {
+  let nameUsr = nameHTML.value;
+  let surnameUsr = surnameHTML.value;
+  let emailUsr = emailHTML.value;
+  let licenseUsr = licenseHTML.value;
+  let specialityUsr = specialityHTML.options[specialityHTML.selectedIndex].text;
+  let passwordUsr = passwordHTML.value;
+  if ( window.location == "./Login.html"){
+    isDoctor = false;
+  }else{
+    isDoctor = true;
+  }
+  let isAprobedUSr = false; 
+
+  let newUser = new User (nameUsr,surnameUsr,emailUsr,licenseUsr,specialityUsr,passwordUsr,isAprobedUSr,isDoctor);
+  console.log(newUser);
+  return newUser
 }
 
 function postear(Object) {
-  fetch("http://localhost:3000/doctors/", {
+  fetch("http://localhost:3000/users/", {
     method: "POST",
     body: JSON.stringify(Object),
     headers: {
@@ -41,16 +50,16 @@ function postear(Object) {
 }
 
 function obtener() {
-  let ObjDoc = newDoctor();
-  fetch("http://localhost:3000/doctors/")
+  let ObjUsr = newUser();
+  fetch("http://localhost:3000/users/")
     .then((response) => response.json())
     .then((response) => {
-      const filter = response.find(email => email.email == ObjDoc.email)
+      const filter = response.find(email => email.email == ObjUsr.email)
       if (filter){
         alert("el mail ya exsiste")
       }else{
-        alert(`Bienvenido ${ObjDoc.name}`)
-        postear(ObjDoc)
+        alert(`Bienvenido ${ObjUsr.name}`)
+        postear(ObjUsr)
       }
     });
 }
